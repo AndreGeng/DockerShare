78b93e937c343d3d4e3f63071faa7b2d042565d2
Andre Geng <andregeng2011@gmail.com>
2019-05-22 11:19:10 +0800

    Updates

---
+++ precache-manifest.6d5f406f21681c21550757237b40ad66.js
@@ -0,0 +1,33 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17ce3519dfe99b57fc5b73b66d22fd25",
+    "url": "index.html"
+  },
+  {
+    "url": "main.1.7aa1eb09e4a0ebc4b3f4.bundle.js"
+  },
+  {
+    "url": "main.1.7aa1eb09e4a0ebc4b3f4.css"
+  },
+  {
+    "url": "presenter.view.2.dd727f9d2edd34a3f36f.bundle.js"
+  },
+  {
+    "url": "runtime.320fedf218483a2bd21d.bundle.js"
+  },
+  {
+    "url": "target-blank.4.be006ab5e769ce0407c3.bundle.js"
+  },
+  {
+    "url": "vendor.5.be6918f910f62cb4e7fc.bundle.js"
+  },
+  {
+    "url": "vendor.5.be6918f910f62cb4e7fc.css"
+  },
+  {
+    "url": "vendors~Sidebar.0.d8b2b0598abe7641b414.bundle.js"
+  },
+  {
+    "url": "vendors~Sidebar.0.d8b2b0598abe7641b414.css"
+  }
+]);
